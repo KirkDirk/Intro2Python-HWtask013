dcf3c206b4608193b6f8db8b11eabcfa5d15ab5d
Kirk Dirk <jackalex@ya.ru>
2022-10-03 09:25:43 +0500

    initial commit and add code

---
+++ HWtask013.py
@@ -0,0 +1,16 @@
+# Задайте список из вещественных чисел.
+# Напишите программу, которая найдёт разницу между
+# максимальным и минимальным значением дробной части элементов.
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+from math import trunc
+from unittest import result
+
+
+floatlist = [1.1, 1.2, 3.1, 5, 10.01]
+templist = []
+for i in range(len(floatlist)):
+    templist.append(floatlist[i] - int(floatlist[i]))
+resultat = max(templist)-min(templist)
+print(trunc(resultat*100)/100)
